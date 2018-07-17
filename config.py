95255ad6d477e3926e5b53538c8ffb61e3621520
ravi828rk <ravi828.rk@gmail.com>
2018-07-17 13:33:37 +0530

    ch 4- database
    flask-sqlalchemy
    flask-migration

--- config.py
+++ config.py
@@ -1,5 +1,12 @@
 import os
+# for database configuration
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
+    # for secret key configuration
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    # database configuration
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False

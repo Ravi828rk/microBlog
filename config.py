caaaa731c31c19fc8c627aa3d5979538aca8dc6e
ravi828rk <ravi828.rk@gmail.com>
2018-07-19 13:07:52 +0530

    ch -10 : email support

--- config.py
+++ config.py
@@ -12,11 +12,18 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # for emailing errors
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    # MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    # MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    # MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    # ADMINS = ['ravi828.rk@gmail.com']
+
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'flasktestingemail@gmail.com'
+    MAIL_PASSWORD = 'flasktestingpassword'
     ADMINS = ['ravi828.rk@gmail.com']
 
     # for pagination
